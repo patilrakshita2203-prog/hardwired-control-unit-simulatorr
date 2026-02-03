145ee22e9e47e314fcd286124ff1b6217c52e885
patilrakshita2203-prog <patilrakshita2203@gmail.com>
2026-02-04 01:53:54 +0530

    Add ControlUnit component for opcode decoding

---
+++ src/ components/ControlUnit.jsx
@@ -0,0 +1,9 @@
+export default function ControlUnit() {
+  return (
+    <div className="box">
+      <h2>Control Unit (Hardwired)</h2>
+      <p>Opcode Decoder + FSM Logic</p>
+      <p>Instruction: ADD R1, R2, R3</p>
+    </div>
+  );
+}
