58a571780168f3dac0b072f8c63acfa597cd039b
patilrakshita2203-prog <patilrakshita2203@gmail.com>
2026-02-04 01:55:07 +0530

    Add ControlSignals component to display control signals

---
+++ src/ components/ControlSignals.jsx
@@ -0,0 +1,14 @@
+export default function ControlSignals() {
+  return (
+    <div className="box">
+      <h2>Control Signals</h2>
+      <ul>
+        <li>RegWrite: 1</li>
+        <li>ALUSrc: 0</li>
+        <li>MemRead: 0</li>
+        <li>MemWrite: 0</li>
+        <li>ALUOp: ADD</li>
+      </ul>
+    </div>
+  );
+}
